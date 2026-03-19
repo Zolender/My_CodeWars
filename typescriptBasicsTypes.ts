a778f0702476cb18c0b6535858db938a209a1832
Eben-Ezer Ndeingar <166811467+Zolender@users.noreply.github.com>
2026-03-19 23:56:56 +0200

    Update typescriptBasicsTypes.ts

--- typescriptBasicsTypes.ts
+++ typescriptBasicsTypes.ts
@@ -9,7 +9,7 @@ export var var7Array = [1, 'test', {a:3}, 4,5]
 export var var8NumericArray: Array<number> = [1,2,3,4,5]
 export var var9Tuple: [string, number] = ['key', 12345]
 export enum Color {Red = 1, Green = 2, Blue = 4}
-export var var10Enum: Color = Color.Blue
+export var var1Enum: Color = Color.Blue
 export var var11ArrayOfAny: Array<any> = [1, 'test', {a: 3}, 4, 5] 
 export function var12VoidFunction():void{} 
 export var var13Null : null = null
